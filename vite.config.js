f10706bb59a32f87b193aeb8129c81e2b5f14921
vincentsandrya <vincentsandrya2000@gmail.com>
2025-05-18 13:07:03 +0700

    commit 2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is set correctly
+  build: {
+    assetsInlineLimit: 0, // Disable inlining small assets
+  }
 })
