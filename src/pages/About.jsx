247424ce48df1431cd96b1e41810a487f2aae5e4
vincentsandrya <vincentsandrya2000@gmail.com>
2025-05-18 09:36:06 +0700

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div style={{ padding: '1rem' }}>
+        <h1>About Page</h1>
+        <p>Welcome to the about page!</p>
+      </div>
+    )
+  }
